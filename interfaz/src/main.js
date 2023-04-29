9406d659bef4d8e0a9544c3fd29af92419c71cf7
Anderly Teleguario <anderlyteleguario@outlook.com>
2023-04-28 22:23:52 -0600

    ajustes interfaz

--- interfaz/src/main.js
+++ interfaz/src/main.js
@@ -2,29 +2,32 @@ import "animate.css";
 import "flatpickr/dist/flatpickr.css";
 import "simplebar/dist/simplebar.min.css";
 import "sweetalert2/dist/sweetalert2.min.css";
+import "vue-good-table-next/dist/vue-good-table-next.css";
+import "vue-toastification/dist/index.css";
+import "./assets/scss/auth.scss";
+import "./assets/scss/tailwind.scss";
+import "v-calendar/dist/style.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
+
+import { useToast } from "vue-toastification";
+import { useThemeSettingsStore } from "@/store/themeSettings";
+import { PiniaSharedState } from 'pinia-shared-state';
+import { createPinia } from 'pinia'
 import {createApp} from "vue";
+
 import VueFlatPickr from "vue-flatpickr-component";
 import VueGoodTablePlugin from "vue-good-table-next";
-import "vue-good-table-next/dist/vue-good-table-next.css";
 import VueSweetalert2 from "vue-sweetalert2";
 import VueTippy from "vue-tippy";
 import Toast from "vue-toastification";
-import "vue-toastification/dist/index.css";
 import VueApexCharts from "vue3-apexcharts";
 import VueClickAway from "vue3-click-away";
 import App from "./App.vue";
-import "./assets/scss/auth.scss";
-import "./assets/scss/tailwind.scss";
 import router from "./router";
 import VCalendar from "v-calendar";
-import {createPinia} from 'pinia'
-import "v-calendar/dist/style.css";
 import axiosClient from "@/plugins/axios.js"
-import { useToast } from "vue-toastification";
-import {useThemeSettingsStore} from "@/store/themeSettings";
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
-import { PiniaSharedState } from 'pinia-shared-state';
-
 
 const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate)
--- interfaz/src/views/index.vue
+++ interfaz/src/views/index.vue
@@ -1,6 +1,9 @@
 <template>
   <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. In necessitatibus quas nesciunt molestiae quae nostrum suscipit quaerat, numquam maiores assumenda consectetur hic animi blanditiis, adipisci ipsum debitis quod placeat ad?</div>
 
+  <div class="alert alert-primary" role="alert">
+    A simple primary alert—check it out!
+  </div>
 </template>
 <script>
 
