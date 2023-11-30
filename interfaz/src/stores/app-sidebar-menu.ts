9680cc90895160fd4a58468a4f20ab64d7d79b13
Aaron Salazar <aaronsalazar99@gmail.com>
2023-11-30 04:41:00 -0600

    Menu y mantenimiento de menu-rol
    Menu Rol y crud creados para asignación de accesos para el menu

--- interfaz/src/stores/app-sidebar-menu.ts
+++ interfaz/src/stores/app-sidebar-menu.ts
@@ -1,26 +1,58 @@
 import { defineStore } from "pinia";
+import axios from 'axios';
+
+let transformedObject: any;
 
 export const useAppSidebarMenuStore = defineStore({
   id: "appSidebarMenu",
   state: () => {
-    return [{
-			text: 'Menú',
-			is_header: true
-		},
-    {
-      'url': '/inicio',
-      'icon': 'fa fa-home',
-      'text': 'Inicio'
-    },
-    {
-      'url': '/usuario',
-      'icon': 'fa fa-user',
-      'text': 'Usuario'
-    },
-    {
-      'url': '/sucursal',
-      'icon': 'fas fa-sitemap',
-      'text': 'Sucursal'
+    return {
+      menuItems: {
+        "0": {
+          'text': 'Menu',
+          'is_header': true,
+          'childs': []
+        }
+      }
     }
-	]}
+  },
+  actions: {
+    async fetchMenu() {
+      try {
+        const res = await axios.get(`http://localhost:8080/api/index.php/menu/buscar`);
+        if (res.data.lista) {
+          const jsonArray = JSON.parse(res.data.lista);
+
+          transformedObject = jsonArray.map((item: { id: any; url: any; icon: any; text: any; level: any; father: any;}) => (
+            {
+              'id': item.id,
+              'url': item.url,
+              'icon': item.icon,
+              'text': item.text,
+              'level': item.level,
+              'father': item.father,
+              'countChilds': 0,
+              'childs': []
+            }
+          ));
+          
+          transformedObject.forEach((item, index) => {
+            if (item.father > 0) {
+              if(this.menuItems.hasOwnProperty(item.father)){
+                this.menuItems[item.father].childs.push(item);
+                this.menuItems[item.father].countChilds += 1;
+              }
+            } else {
+              this.menuItems[item.id] = item;
+            }
+            
+          });
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
 });
+
+useAppSidebarMenuStore().fetchMenu();
